347c2c224f263a44acac3a8250992c734b4256ad
Vadym <vadymlinevych@gmail.com>
2015-04-15 21:24:43 +0300

    new changes

--- JavaCode/Lesson15_Multithreading/src/Task_04/ThreadGetInfo.java
+++ JavaCode/Lesson15_Multithreading/src/Task_04/ThreadGetInfo.java
@@ -1,21 +1,19 @@
 package Task_04;
 
-/**
- * Created by Admin on 4/14/2015.
- */
 public class ThreadGetInfo extends Thread {
-
-    ThreadGetInfo(ThreadGroup tg, String name) { //name of thread and group
-        super(tg, name);
-    }
 
     @Override
     public void run() {
-        System.out.println(getName() + " | " + getId()); // вывод на консоль имени и номера потока
-        while (!isInterrupted()) ; // ожидание завершения
-
+        System.out.println(getName() + " | " + getId());
+        while (!isInterrupted()) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(getName() + " interrupted");
+                return;
+            }
+        }
     }
-
 }
 
 
