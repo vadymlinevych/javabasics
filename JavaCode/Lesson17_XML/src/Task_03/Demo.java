ba0e2d87d9751632618baf7f16f1b5477dfcdf0c
Vadym <vadymlinevych@gmail.com>
2015-04-22 20:14:13 +0300

    new changes

--- JavaCode/Lesson17_XML/src/Task_03/Demo.java
+++ JavaCode/Lesson17_XML/src/Task_03/Demo.java
@@ -1,8 +1,10 @@
 package Task_03;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.util.Scanner;
 
 /**
  3. Есть список поездов, представленный с виде XML. Вывести на экран информацию о тех поездах, которые отправляются сегодня с 15:00 до 19:00.
@@ -30,7 +32,30 @@ public class Demo {
             String timeFrom = "15:00";
             String timeTo = "19:00";
 
+            //1
             trains.findTrain(timeFrom, timeTo);
+
+
+            //
+            Scanner scan = new Scanner(System.in);
+            System.out.println("ID:");
+            String id = scan.nextLine();
+            System.out.println("Input train from:");
+            String trainFrom = scan.nextLine();
+            System.out.println("Input train to:");
+            String trainTo = scan.nextLine();
+            System.out.println("Input date departure (dd.mm.yyyy):");
+            String date = scan.nextLine();
+            System.out.println("Input time departure:");
+            String timeDep = scan.nextLine();
+
+            trains.add(new Train(id, trainFrom, trainTo, date, timeDep));
+
+
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(trains, f);
+
 
         } catch(Exception ex) {
             ex.printStackTrace();
