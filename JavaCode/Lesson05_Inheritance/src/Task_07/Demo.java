7a104d2612fd215ff72febe98a8c6e430fbb9604
Vadym <vadymlinevych@gmail.com>
2015-05-06 19:23:56 +0300

    new changes

--- JavaCode/Lesson05_Inheritance/src/Task_07/Demo.java
+++ JavaCode/Lesson05_Inheritance/src/Task_07/Demo.java
@@ -7,5 +7,24 @@ package Task_07;
 public class Demo {
  public static void main(String args[]) {
 
+  Developer[] list = {
+          new JuniorDeveloper("Mike", 500, 0),
+          new JuniorDeveloper("Dima", 500, 1),
+          new JuniorDeveloper("Vova", 500, 2),
+          new SeniorDeveloper("Jack", 500, 5),
+          new TeamLead("Robby", 500, 7)
+  };
+  StringBuilder sb;
+
+  for (Developer d : list) {
+   sb = new StringBuilder() // !!!
+           .append(d.getName())
+           .append(": ")
+           .append(d.getBasicSalary())
+           .append(" -> ")
+           .append(d.getSalary());
+
+   System.out.println(sb.toString());
+  }
  }
 }
---
+++ JavaCode/Lesson05_Inheritance/src/Task_07/SeniorDeveloper.java
@@ -0,0 +1,12 @@
+package Task_07;
+public class SeniorDeveloper extends Developer {
+
+	public SeniorDeveloper(String name, int basicSalary, int experience) {
+		super(name, basicSalary, experience);
+	}
+
+	@Override
+	public double getSalary() {
+		return (basicSalary * 2) + (experience > 0 ? basicSalary * experience * 0.1 : 0);
+	}
+}
